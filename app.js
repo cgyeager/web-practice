31cee0acf80e37a120a9fa7c5289e27e2d1b9e75
chris <chris@email.com>
2024-06-23 08:39:26 -0400

    fixes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const menuOpen = document.querySelector('#mainMenuOpen');
 const navButtons = document.querySelector('.nav-buttons');
 
+navButtons.style.display = "none";
+
 menuOpen.addEventListener('click', () => {
   if (navButtons.style.display === "none") {
     navButtons.style.display = "flex";
